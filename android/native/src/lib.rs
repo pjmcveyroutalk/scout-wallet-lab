cab643e7c40327f6ca3b1df41e92bbfa12dc094c
pjmcveyroutalk <pjmcvey.routalk@gmail.com>
2026-09-05 19:59:40 -0500

    Change unsafe code handling and simplify imports

--- android/native/src/lib.rs
+++ android/native/src/lib.rs
@@ -1,15 +1,12 @@
-#![forbid(unsafe_code)]
+#![deny(unsafe_code)]
 
-use jni::{
-    objects::{JClass, JString},
-    sys::jstring,
-    JNIEnv,
-};
+use jni::{objects::JClass, sys::jstring, JNIEnv};
 use wallet_engine::engine_name;
 
+#[allow(unsafe_code)]
 #[no_mangle]
 pub extern "system" fn Java_com_routalk_scoutoperator_NativeBridge_engineName(
-    mut env: JNIEnv<'_>,
+    env: JNIEnv<'_>,
     _class: JClass<'_>,
 ) -> jstring {
     let value = format!("{}:devnet", engine_name());
@@ -20,9 +17,10 @@ pub extern "system" fn Java_com_routalk_scoutoperator_NativeBridge_engineName(
     }
 }
 
+#[allow(unsafe_code)]
 #[no_mangle]
 pub extern "system" fn Java_com_routalk_scoutoperator_NativeBridge_bridgeStatus(
-    mut env: JNIEnv<'_>,
+    env: JNIEnv<'_>,
     _class: JClass<'_>,
 ) -> jstring {
     let value = "wallet-operations-locked";
