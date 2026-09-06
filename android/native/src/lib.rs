4aad6062e565c2cbbafc4ae4a90a7add11687882
pjmcveyroutalk <pjmcvey.routalk@gmail.com>
2026-09-05 20:19:23 -0500

    Refactor JNI functions to use java_string helper

--- android/native/src/lib.rs
+++ android/native/src/lib.rs
@@ -1,16 +1,9 @@
 #![deny(unsafe_code)]
 
 use jni::{objects::JClass, sys::jstring, JNIEnv};
-use wallet_engine::engine_name;
+use wallet_engine::{engine_name, Cluster};
 
-#[allow(unsafe_code)]
-#[no_mangle]
-pub extern "system" fn Java_com_routalk_scoutoperator_NativeBridge_engineName(
-    env: JNIEnv<'_>,
-    _class: JClass<'_>,
-) -> jstring {
-    let value = format!("{}:devnet", engine_name());
-
+fn java_string(mut env: JNIEnv<'_>, value: &str) -> jstring {
     match env.new_string(value) {
         Ok(output) => output.into_raw(),
         Err(_) => std::ptr::null_mut(),
@@ -19,14 +12,37 @@ pub extern "system" fn Java_com_routalk_scoutoperator_NativeBridge_engineName(
 
 #[allow(unsafe_code)]
 #[no_mangle]
+pub extern "system" fn Java_com_routalk_scoutoperator_NativeBridge_engineName(
+    env: JNIEnv<'_>,
+    _class: JClass<'_>,
+) -> jstring {
+    let value = format!("{}:{}", engine_name(), Cluster::Devnet.rpc_name());
+    java_string(env, &value)
+}
+
+#[allow(unsafe_code)]
+#[no_mangle]
 pub extern "system" fn Java_com_routalk_scoutoperator_NativeBridge_bridgeStatus(
     env: JNIEnv<'_>,
     _class: JClass<'_>,
 ) -> jstring {
-    let value = "wallet-operations-locked";
+    java_string(env, "wallet-operations-locked")
+}
 
-    match env.new_string(value) {
-        Ok(output) => output.into_raw(),
-        Err(_) => std::ptr::null_mut(),
-    }
+#[allow(unsafe_code)]
+#[no_mangle]
+pub extern "system" fn Java_com_routalk_scoutoperator_NativeBridge_rpcCluster(
+    env: JNIEnv<'_>,
+    _class: JClass<'_>,
+) -> jstring {
+    java_string(env, Cluster::Devnet.rpc_name())
+}
+
+#[allow(unsafe_code)]
+#[no_mangle]
+pub extern "system" fn Java_com_routalk_scoutoperator_NativeBridge_rpcEndpoint(
+    env: JNIEnv<'_>,
+    _class: JClass<'_>,
+) -> jstring {
+    java_string(env, Cluster::Devnet.rpc_url())
 }
